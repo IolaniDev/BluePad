2e425689a9d69f8e313f8c526c402ad6524281fd
student <student@CART115-LAP03.local>
2015-04-22 17:37:37 -1000

    Initial Commit

---
+++ BluePad2.0/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BluePad2.0
+//
+//  Created by student on 4/22/15.
+//  Copyright (c) 2015 Iolani. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
